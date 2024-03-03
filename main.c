565fe4098ab2766798a57abaa8e4162d109f5887
Morfey <stalker22813372@mail.ru>
2024-03-03 21:14:40 +0300

    refactoring / bug fix

--- main.c
+++ main.c
@@ -1,188 +1,157 @@
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 #include "libs/data_structures/vector/vector.h"
 #include "libs/data_structures/vectorVoid/vectorVoid.h"
 
 // тест на работоспособность функции create
-void test_createVector_non_empty_vector(){
-    vector v = createVector(5);
+void test_createVectorV_int(){
+    vectorVoid v = createVectorV(5, sizeof(int));
 
     assert(v.data != NULL && v.size == 0 && v.capacity == 5);
 
-    deleteVector(&v);
+    deleteVectorV(&v);
 }
 
-void test_createVector_empty_vector(){
-    vector v = createVector(0);
+void test_createVectorV_float_zero_length(){
+    vectorVoid v = createVectorV(0, sizeof(float));
 
     assert(v.size == 0 && v.capacity == 0);
 
-    deleteVector(&v);
+    deleteVectorV(&v);
 }
 
-void test_createVector(){
-    test_createVector_non_empty_vector();
-    test_createVector_empty_vector();
+void test_createVectorV(){
+    test_createVectorV_int();
+    test_createVectorV_float_zero_length();
 }
 
 // тест на изменение размера вектора
-void test_reserve(){
-    vector v = createVector(5);
+void test_reserveV(){
+    vectorVoid v = createVectorV(5, sizeof(int));
 
-    reserve(&v, 10);
+    reserveV(&v, 10);
     assert(v.capacity == 10);
 
-    reserve(&v, 7);
+    reserveV(&v, 7);
     assert(v.capacity == 7);
 
-    reserve(&v, 0);
+    reserveV(&v, 0);
     assert(v.capacity == 0);
 
-    deleteVector(&v);
+    deleteVectorV(&v);
 }
 
 // тест на добавление в конец вектора элемент
-void test_pushBack_non_empty_vector() {
-    vector v = createVector(6);
+void test_pushBackV_non_empty_vector_int() {
+    vectorVoid v = createVectorV(6, sizeof(int));
 
-    pushBack(&v, 1);
-    pushBack(&v, 2);
-    pushBack(&v, 3);
-    pushBack(&v, 4);
-    pushBack(&v, 5);
-    assert(v.size == 5);
+    int x[3] = {1, 2, 3};
+    for (int i = 0; i < 3; i++){
+        pushBackV(&v, x + i);
+    }
 
-    deleteVector(&v);
+    for (int i = 0; i < 3; i++){
+        int value;
+        getVectorValueV(&v, i, &value);
+
+        assert(value == x[i]);
+    }
+
+    deleteVectorV(&v);
 }
 
-void test_pushBack_empty_vector() {
-    vector v = createVector(5);
+void test_pushBackV_non_empty_vector_float() {
+    vectorVoid v = createVectorV(6, sizeof(float));
 
-    pushBack(&v, 5);
-    assert(v.data[0] == 5 && v.size == 1);
+    float x[3] = {1, 2, 3};
+    for (int i = 0; i < 3; i++){
+        pushBackV(&v, x + i);
+    }
 
-    deleteVector(&v);
+    for (int i = 0; i < 3; i++){
+        float value;
+        getVectorValueV(&v, i, &value);
+
+        assert(fabs(value - x[i]) < 0.0001);
+    }
+
+    deleteVectorV(&v);
+}
+
+void test_pushBackV_empty_vector() {
+    vectorVoid v = createVectorV(5, sizeof(int));
+
+    int x = 5;
+    pushBackV(&v, &x);
+
+    int value;
+    getVectorValueV(&v, 0, &value);
+
+    assert(value == x && v.size == 1 && v.baseTypeSize == sizeof(int));
+
+    deleteVectorV(&v);
 }
 
 void test_pushBack_zero_length() {
-    vector v = createVector(0);
+    vectorVoid v = createVectorV(0, sizeof(int));
 
-    pushBack(&v, 5);
-    assert(v.data[0] == 5 && v.size == 1 && v.capacity == 1);
+    int x = 5;
+    pushBackV(&v, &x);
 
-    deleteVector(&v);
+    int value;
+    getVectorValueV(&v, 0, &value);
+
+    assert(value == x && v.size == 1 && v.baseTypeSize == sizeof(int));
+
+    deleteVectorV(&v);
 }
 
 void test_pushBack(){
-    test_pushBack_non_empty_vector();
-    test_pushBack_empty_vector();
+    test_pushBackV_non_empty_vector_int();
+    test_pushBackV_non_empty_vector_float();
+    test_pushBackV_empty_vector();
     test_pushBack_zero_length();
 }
 
 // тест на освобождение памяти, выделенную под неиспользуемые элементы
-void test_shrinkToFit(){
-    vector v = createVector(7);
+void test_shrinkToFitV(){
+    vectorVoid v = createVectorV(7, sizeof(int));
 
-    for (int i = 1; i < 6; i++)
-        pushBack(&v, i);
+    int x = 5;
+    for (int i = 1; i < 5; i++)
+        pushBackV(&v, &x);
 
-    shrinkToFit(&v);
+    shrinkToFitV(&v);
     assert(v.capacity == v.size);
 
-    deleteVector(&v);
+    deleteVectorV(&v);
 }
 
 // тест на удаление последнего элемента
 void test_pop_back() {
-    vector v = createVector(0);
+    vectorVoid v = createVectorV(0, sizeof(int));
 
-    pushBack(&v, 3);
+    int x = 3;
+    pushBackV(&v, &x);
     assert(v.size == 1);
 
-    popBack(&v);
+    popBackV(&v);
     assert(v.size == 0 && v.capacity == 1);
 
-    deleteVector(&v);
+    deleteVectorV(&v);
 }
 
-// тест на получение адреса i-го элемента вектора
-void test_at_vector_non_empty_vector() {
-    vector v = createVector(5);
-
-    pushBack(&v, 6);
-    pushBack(&v, 4);
-    pushBack(&v, 8);
-
-    int* ptr = atVector(&v, 1);
-    assert(v.data[1] == *ptr);
-
-    deleteVector(&v);
-}
-void test_at_vector_request_to_last_element() {
-    vector v = createVector(4);
-
-    pushBack(&v, 1);
-    pushBack(&v, 3);
-    pushBack(&v, 8);
-    pushBack(&v, 6);
-
-    int* ptr = back(&v);
-    assert(v.data[3] == *ptr);
-
-    deleteVector(&v);
-}
-
-void test_at_vector_request_to_first_element() {
-    vector v = createVector(4);
-
-    pushBack(&v, 1);
-    pushBack(&v, 3);
-    pushBack(&v, 8);
-    pushBack(&v, 6);
-
-    int* ptr = front(&v);
-    assert(v.data[0] == *ptr);
-
-    deleteVector(&v);
-}
-
-void test_at_vector() {
-    test_at_vector_non_empty_vector();
-    test_at_vector_request_to_last_element();
-    test_at_vector_request_to_first_element();
+void test(){
+    test_createVectorV();
+    test_reserveV();
+    test_pushBack();
+    test_shrinkToFitV();
+    test_pop_back();
 }
 
 int main() {
-    size_t n;
-    scanf("%zd", &n);
-
-    vectorVoid v = createVectorV(0, sizeof(int));
-    for (int i = 0; i < n; i++) {
-        int x;
-        scanf("%d", &x);
-        pushBackV(&v, &x);
-    }
-
-    for (int i = 0; i < n; i++) {
-        int x;
-        getVectorValueV(&v, i, &x);
-        printf("%d ", x);
-    }
-
-    printf("\n");
-
-    vectorVoid vz = createVectorV(0, sizeof(float));
-    for (int i = 0; i < n; i++) {
-        float x;
-        scanf("%f", &x);
-        pushBackV(&vz, &x);
-    }
-
-    for (int i = 0; i < n; i++) {
-        float x;
-        getVectorValueV(&vz, i, &x);
-        printf("%f ", x);
-    }
+    test();
 
     return 0;
 }
