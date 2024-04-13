7744cb9536239def2a7ed22ff5c71f5af204e745
Morfey <stalker22813372@mail.ru>
2024-04-13 17:17:12 +0300

    realization strlen_

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
-
+#include "string/tasks/strlen_.h"
 
 int main() {
+    test_strlen_();
 
     return 0;
 }
---
+++ string/string_.c
@@ -0,0 +1,9 @@
+#include "string_.h"
+
+size_t strlen_(const char* begin){
+    char *end = begin;
+    while (*end != '\0')
+        end++;
+
+    return end - begin;
+}
