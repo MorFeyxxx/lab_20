21c94562c0d36a73726a52ac3725f3126fa6691e
Morfey <stalker22813372@mail.ru>
2024-04-27 14:44:01 +0300

    first commit

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
-#include "string/tasks/strlen_.h"
-#include "string/tasks/search functions.h"
-#include "string/tasks/strcmp_.h"
-#include "string/tasks/Functions for copying.h"
+#include <stdio.h>
+
 
 int main() {
-    test_strlen_();
-    test_find();
-    test_find_non_space();
-    test_find_space();
-    test_find_non_space_reverse();
-    test_find_space_reverse();
-    test_strcmp_();
-    test_copy();
-    test_copy_if();
-    test_copy_if_reverse();
-
     return 0;
 }
 
