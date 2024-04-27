0a78a5d1cc895809838774c7691770d4f068db0e
Morfey <stalker22813372@mail.ru>
2024-04-27 16:33:33 +0300

    remove_adjacent_equal_letters

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "string/tasks/remove_non_letters.h"
+#include "string/tasks/remove_adjacent_equal_letters.h"
 
 int main() {
     test_remove_non_letters();
+    test_2_remove_adjacent_equal_letters();
     return 0;
 }
 
