b8783826a47d9305bd7235de3f35373bee931539
Morfey <stalker22813372@mail.ru>
2024-05-12 13:20:39 +0300

    filter_word

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "libs/algorithms/matrix_transposition.h"
 #include "libs/algorithms/convert_float.h"
 #include "libs/algorithms/evaluate_expression.h"
+#include "libs/algorithms/filter_word.h"
 
 int main(){
     test_matrix_transpose();
     test_convert_float();
     test_evaluate();
+    test_filter_word();
 }
